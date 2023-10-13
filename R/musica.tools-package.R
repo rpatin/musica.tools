421521175c153ab1703769bed4d69e36374ab21c
Remi Lemaire-Patin <remi.lemaire-patin@inrae.fr>
2023-10-13 09:17:19 +0200

    Add basic folder structure

---
+++ R/musica.tools-package.R
@@ -0,0 +1,8 @@
+#' musica.tools: tools to extract and plot MuSICA outputs
+#'
+#' Provides several function to extract MuSICA output from netcdf files.
+#' Output can then be used to produce a series of generic plots.
+#'
+#' @docType package
+#' @name musica.tools
+NULL
