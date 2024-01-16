5fe8f5b07bab7e32a3c18cce6456d3d4c8960717
Remi Lemaire-Patin <remi.lemaire-patin@inrae.fr>
2024-01-16 10:47:25 +0100

    added global variables

---
+++ R/globalVariables.R
@@ -0,0 +1,47 @@
+
+# .check_ggplot_variable --------------------------------------------------
+utils::globalVariables(names = c("time"))
+
+# dygraph_comparison --------------------------------------------------
+utils::globalVariables(names = c("time"))
+
+# dygraph_variable --------------------------------------------------
+utils::globalVariables(names = c("time"))
+
+# get_air_level --------------------------------------------------
+utils::globalVariables(names = c("veget_height_top",
+                                 "nair",
+                                 "relative_height",
+                                 "layer_thickness"))
+
+# get_soil_level --------------------------------------------------
+utils::globalVariables(names = c("nsoil",
+                                 "z_soil",
+                                 "dz_soil"))
+
+# get_two_variables --------------------------------------------------
+utils::globalVariables(names = c("remove_model_col"))
+
+# get_variable --------------------------------------------------
+utils::globalVariables(names = c("time"))
+
+# ggplot_variable --------------------------------------------------
+utils::globalVariables(names = c("diff.legend",
+                                 "time",
+                                 "datenum",
+                                 "datepos",
+                                 "label",
+                                 "veget_height_top",
+                                 "relative_height"))
+
+# get_air_level --------------------------------------------------
+utils::globalVariables(names = c("veget_height_top"))
+
+# get_air_level --------------------------------------------------
+utils::globalVariables(names = c("veget_height_top"))
+
+# get_air_level --------------------------------------------------
+utils::globalVariables(names = c("veget_height_top"))
+
+# get_air_level --------------------------------------------------
+utils::globalVariables(names = c("veget_height_top"))
