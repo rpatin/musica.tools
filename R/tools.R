8348ab12191182cbd8c55cc9005c3883b12e9bde
Remi Lemaire-Patin <remi.lemaire-patin@inrae.fr>
2024-02-15 13:36:50 +0100

    fixed attr_legend when >1 models involved

--- R/tools.R
+++ R/tools.R
@@ -168,7 +168,12 @@ format_time <- function(time, date0) {
 attr_legend <- function(df) {
   this.units <- attr(df,"units")
   this.longname <- attr(df, "longname")
-  if (!is.null(this.units) && this.units != "") {
+  if (length(this.units) == 2) {
+    this.units <- ifelse(this.units == "", "-", this.units)
+    return( 
+      paste0(this.longname, " (", this.units,")") 
+      )
+  } else if (!is.null(this.units) && this.units != "") {
     return(
       paste0(this.longname, " (", this.units,")")
     )
